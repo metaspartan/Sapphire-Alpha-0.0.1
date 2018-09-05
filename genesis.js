0117c1f6a886a83069d0011ee684fabb0fee2480
ftriantos <31746846+ftriantos@users.noreply.github.com>
2018-09-05 16:54:13 -0400

    hashing files worked

--- genesis.js
+++ genesis.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const sha256 = require('crypto-js/sha256');
-const md5File = require('md5-file');
+//const md5File = require('md5-file');
 /////////////////////////////////////////////////////////////////welcome message
 console.log("EtherGem Sapphire Integrated Subchain with DEX circa 2018");
 ////////////////////////////////////////////////////////////////////////tag line
@@ -10,17 +10,17 @@ var fileHash = "";
 var filename = "peer.js";
 var tbh = "";
 
-const hash = md5File.sync(filename);
+//const hash = md5File.sync(filename);
 
 var getFiseHash = function(){
   fs.readFile(filename, 'utf8', function(err, data) {
       if (err) throw err;
-      //tbh=data;
-      tbh = data.slice(data.indexOf("const fs ="), data.indexOf("fileHash:getFiseHash")).replace(/(\r\n|\n|\r)/gm,"");;
+      tbh=data.replace(/(\r\n|\n|\r)/gm,"");
+      //tbh = data.slice(data.indexOf("const fs ="), data.indexOf("fileHash:getFiseHash")).replace(/(\r\n|\n|\r)/gm,"");
       //console.log("output is"+tbh);
       fileHash = sha256(tbh).toString();
       console.log("filehash is "+fileHash);
-      console.log("md5 is "+hash);
+      //console.log("md5 is "+hash);
   });
 }
 //////////////////////////////////////////////////////////////////end hash files
