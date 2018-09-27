eeac29aee44d0ff0b516a034b5d70d1dca4190e9
ftriantos <31746846+ftriantos@users.noreply.github.com>
2018-09-27 09:49:31 -0400

    disable rpc for testing

--- rpc_server.js
+++ rpc_server.js
@@ -148,7 +148,7 @@ function requestListener(request, response) {
 }
 
 console.log(`starting the server on port ${PORT}`);
-server.listen(PORT);
+//server.listen(PORT);
 
 module.exports = {
   globalParentCom:globalParentCom
