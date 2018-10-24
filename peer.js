f60cfd14b22a500f71ccec150fe95252f0405947
ftriantos <31746846+ftriantos@users.noreply.github.com>
2018-10-24 18:00:57 -0400

    peer connection gets lost

--- peer.js
+++ peer.js
@@ -258,7 +258,7 @@ function isJSON(str) {
               ****/
             //setting a delay and pong back
             //setTimeout(function(){peers[peerId].conn.write("ChainSyncPong("+peerBlockHeight+")");},5000);
-            if(peers[peerId].conn){
+            if(peers[peerId]){
               setTimeout(function(){peers[peerId].conn.write(JSON.stringify({"ChainSyncPong":{Height:peerBlockHeight,MaxHeight:frankieCoin.getLength(),GlobalHash:globalGenesisHash}}));},300);
             }
             //peers[peerId].conn.write(JSON.stringify(frankieCoin.getLatestBlock()));
