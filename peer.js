b0bb158e13490aebc9c471324ed41ea578fac0f2
ftriantos <31746846+ftriantos@users.noreply.github.com>
2018-12-08 07:44:32 -0500

    error chasing

--- peer.js
+++ peer.js
@@ -935,7 +935,7 @@ var broadcastPeersBlock = function(){
   log("------------------------------------------------------")
   log(chalk.bgGreen("BROADCASTING QUARRY MINED BLOCK TO PEERS"))
   log("------------------------------------------------------")
-  broadcastPeers(frankieCoin.getLatestBlock());
+  broadcastPeers(JSON.stringify(frankieCoin.getLatestBlock()));
 }
 
 //parent communicator callback function sent to child below
