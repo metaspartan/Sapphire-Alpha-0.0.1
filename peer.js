af31b887b70fd03f02a14d215ec4eabdb808f237
ftriantos <31746846+ftriantos@users.noreply.github.com>
2018-12-22 23:35:52 -0500

    blockstream testing

--- peer.js
+++ peer.js
@@ -343,7 +343,7 @@ var addyBal = function(val){
               if(frankieCoin.getLength() > parseInt(peerBlockHeight) && (frankieCoin.getLength() - parseInt(peerBlockHeight)) / parseInt(frankieCoin.chainRiser) > 0){
                 console.log("this is properly flagged for streaming");
                 var pongBackBlockStream = function(blockData){
-                  peers[peerId].conn.write(JSON.stringify({"pongBlockStream":blockData.toString()}));
+                  peers[peerId].conn.write({"pongBlockStream":[blockData.toString()]});
                 }
                 BlkDB.getBlockStream(parseInt(peerBlockHeight),pongBackBlockStream);
                 //pongBack = true;//not sure about this since this is a stream
