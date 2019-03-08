5033fcd55a00a4e3aa9605f0f36814d05f2f47d4
osoese <ftriantos@gmail.com>
2019-03-08 14:07:15 -0500

    testing

--- peer.js
+++ peer.js
@@ -1007,11 +1007,11 @@ function cliGetInput(){
 
       console.log("SECRET MESSAGE BEGINNINGS BRO "+secretPeerMSG);
       for (let i in frankieCoin.nodes){
-
-        var nodeSecret = frankieCoin.nodes[i]["thisNodeSecret"];
-        peers[frankieCoin.nodes[i]["id"]].conn.write(JSON.stringify({peerSafe:{message:"SECRET MESSAGE BEGINNINGS BRO "+secretPeerMSG+nodeSecret}}));
-        //broadcastPeers(JSON.stringify({peerSafe:{message:"SECRET MESSAGE BEGINNINGS BRO "+secretPeerMSG+encrypted.toString(hex)}}));
-
+        if(peers[frankieCoin.nodes[i]["id"]].conn){
+          var nodeSecret = frankieCoin.nodes[i]["thisNodeSecret"];
+          peers[frankieCoin.nodes[i]["id"]].conn.write(JSON.stringify({peerSafe:{message:"SECRET MESSAGE BEGINNINGS BRO "+secretPeerMSG+nodeSecret}}));
+          //broadcastPeers(JSON.stringify({peerSafe:{message:"SECRET MESSAGE BEGINNINGS BRO "+secretPeerMSG+encrypted.toString(hex)}}));
+        }
       }
       cliGetInput();
     }else if(userInput == "TX"){
