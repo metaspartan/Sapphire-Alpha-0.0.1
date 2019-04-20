ae2d92fb7c2826de9656a087c41ae05120735f31
osoese <ftriantos@gmail.com>
2019-04-20 18:43:13 -0400

    conn 1 for nodestate

--- peer.js
+++ peer.js
@@ -146,7 +146,7 @@ var activePing = function(){
       log(chalk.green("Sending ping for peer id "+id));
       log("------------------------------------------------------");
 
-        peers[id].conn2.write(JSON.stringify({"nodeStatePing":{Height:parseInt(chainState.synchronized),MaxHeight:parseInt(chainState.synchronized),GlobalHash:globalGenesisHash}}));
+        peers[id].conn.write(JSON.stringify({"nodeStatePing":{Height:parseInt(chainState.synchronized),MaxHeight:parseInt(chainState.synchronized),GlobalHash:globalGenesisHash}}));
 
     }
   }
@@ -1150,6 +1150,7 @@ let connSeq2 = 0
 
         }else if(JSON.parse(data)["nodeStatePong"]){
 
+          console.log(JSON.parse(data)["nodeStatePong"]);
           if(JSON.parse(data)["nodeStatePong"]["GlobalHash"] == globalGenesisHash){//will add more to this
             frankieCoin.incrementPeerMaxHeight(peerId,JSON.parse(data)["nodeStatePong"]["MaxHeight"]);
             BlkDB.addNode("node:"+peerId+":MaxHeight",JSON.parse(data)["nodeStatePong"]["MaxHeight"]);
@@ -1157,10 +1158,11 @@ let connSeq2 = 0
 
         }else if(JSON.parse(data)["nodeStatePing"]){
 
+          console.log(JSON.parse(data)["nodeStatePing"]);
           if(JSON.parse(data)["nodeStatePing"]["GlobalHash"] == globalGenesisHash){//will add more to this
             frankieCoin.incrementPeerMaxHeight(peerId,JSON.parse(data)["nodeStatePing"]["MaxHeight"]);
             BlkDB.addNode("node:"+peerId+":MaxHeight",JSON.parse(data)["nodeStatePing"]["MaxHeight"]);
-            peers[peerId].conn2.write(JSON.stringify({"nodeStatePong":{Height:parseInt(chainState.synchronized),MaxHeight:parseInt(chainState.synchronized),GlobalHash:globalGenesisHash}}));
+            peers[peerId].conn.write(JSON.stringify({"nodeStatePong":{Height:parseInt(chainState.synchronized),MaxHeight:parseInt(chainState.synchronized),GlobalHash:globalGenesisHash}}));
           }
 
         }else if(JSON.parse(data)["ChainSyncPing"]){
