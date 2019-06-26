1c6bfdf29a98be5a6e535e1d89bbeaac5c50dedc
osoese <ftriantos@gmail.com>
2019-06-26 06:57:14 -0400

    testing

--- peer.js
+++ peer.js
@@ -632,6 +632,11 @@ var getConnectionConfig = async function(ntwk){
       myLastSessionId = npid;
       console.log("my last session = "+myLastSessionId);
       if(myLastSessionId != "notfound"){
+        for(i=1;i<10000;i++){
+          if(i%1000 == 0){
+            console.log("reloading connection "+i);
+          }
+        }
         chainState.nodePersistantId = myLastSessionId;
         console.log("node persistantce was already set ")
       }else{
@@ -1447,11 +1452,7 @@ var cbReset = async function(){
     port2 = await getPort()
 
     sw = await getConnectionConfig(this);
-    for(i=1;i<5000;i++){
-      if(i%1000 == 0){
-        console.log("reloading connection "+i);
-      }
-    }
+
     sw2 = await getConnectionConfig(this);
 
     sw.listen(port)//peers
