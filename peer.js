9c8a67f5d56cdc22d2fc016986169a32abcdea30
unknown <ftriantos@gmail.com>
2019-07-28 10:53:13 -0400

    test

--- peer.js
+++ peer.js
@@ -2136,7 +2136,7 @@ var cbReset = async function(full = false){
           console.log("is json")
           console.log(data.toString().substr(0,500));
           if( (JSON.parse(data)["checkPointHash"] && JSON.parse(data)["currentBlockCheckPointHash"] && JSON.parse(data)["block"])
-          || (JSON.parse(data)["nodeStatePing"]["checkPointHash"] && JSON.parse(data)["nodeStatePing"]["currentBlockCheckPointHash"] && JSON.parse(data)["nodeStatePing"]["block"])
+          || (JSON.parse(data)["nodeStatePing"] && JSON.parse(data)["nodeStatePing"]["checkPointHash"] && JSON.parse(data)["nodeStatePing"]["currentBlockCheckPointHash"] && JSON.parse(data)["nodeStatePing"]["block"])
           ){
             console.log("IT SHOWS IT HERE");
           }
@@ -2272,7 +2272,7 @@ var cbReset = async function(full = false){
           //if(JSON.parse(data)["previousHash"]){/////////need more refinement
           //{checkPointHash:chainState.checkPointHash,currentBlockCheckPointHash:chainState.currentBlockCheckPointHash,block:frankieCoin.getLatestBlock()}
           if( (JSON.parse(data)["checkPointHash"] && JSON.parse(data)["currentBlockCheckPointHash"] && JSON.parse(data)["block"])
-          || (JSON.parse(data)["nodeStatePing"]["checkPointHash"] && JSON.parse(data)["nodeStatePing"]["currentBlockCheckPointHash"] && JSON.parse(data)["nodeStatePing"]["block"])
+          || (JSON.parse(data)["nodeStatePing"] && JSON.parse(data)["nodeStatePing"]["checkPointHash"] && JSON.parse(data)["nodeStatePing"]["currentBlockCheckPointHash"] && JSON.parse(data)["nodeStatePing"]["block"])
           ){
 
             console.log("INCOMING block "+JSON.parse(data));
